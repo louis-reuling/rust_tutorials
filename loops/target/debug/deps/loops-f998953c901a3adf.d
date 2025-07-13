c9e040d2cf70b7038d8da3312662b2cafd18907e
louis-reuling <dev.louisreuling@gmail.com>
2025-07-13 22:00:33 +0200

    loop within a variable

---
+++ loops/target/debug/deps/loops-f998953c901a3adf.d
@@ -0,0 +1,5 @@
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/loops-f998953c901a3adf.d: src/main.rs
+
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/loops-f998953c901a3adf: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/loops: /home/dev/dev/02_learning/rust/rust_tutorial/loops/src/main.rs
