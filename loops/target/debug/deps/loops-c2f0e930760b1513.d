c1c73cbb99d4a5767fc59383ebcb6733a5e21644
louis-reuling <dev.louisreuling@gmail.com>
2025-07-13 21:58:13 +0200

    added loops project

---
+++ loops/target/debug/deps/loops-c2f0e930760b1513.d
@@ -0,0 +1,5 @@
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/loops-c2f0e930760b1513.d: src/main.rs
+
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/libloops-c2f0e930760b1513.rmeta: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/deps/loops-d9de21c12722a984.d
@@ -0,0 +1,5 @@
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/loops-d9de21c12722a984.d: src/main.rs
+
+/home/dev/dev/02_learning/rust/rust_tutorial/loops/target/debug/deps/libloops-d9de21c12722a984.rmeta: src/main.rs
+
+src/main.rs:
